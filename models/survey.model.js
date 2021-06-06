2fa3392e7b38e2c1766b3e7775d7fe4a44e06ca8
Philip Campani <philip@Philips-MacBook-Pro.local>
2021-06-06 13:51:15 +0800

    Modified comments

--- models/survey.model.js
+++ models/survey.model.js
@@ -48,7 +48,7 @@ class SurveyModel extends Model {
 	generateCaptcha(){
 		return this.captcha;
 	}
-	// Verify input captcha in the server again
+	// Verify input captcha in the server again and again
 	verifyCaptchaInput(input){
 		if(this.generateCaptcha() === input) {
 			return "Success! Captcha input matched.";
